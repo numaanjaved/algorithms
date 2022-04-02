765132ed5ccbdc57589d2f4bc565fbfb068276a3
Numaan Javed <naumaan.javed@hotmail.com>
2022-04-02 11:14:27 +0500

    mergesort updated

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -2,68 +2,63 @@
 using namespace std;
 
 void merge(int array[], int left, int mid, int right){
-    if(left < right){
-        cout << endl;
-        cout << "left = " << left << endl;
-        cout << "right = " << right << endl;
-        cout << "mid = " << mid << endl;
 
-        int leftArrayLength = mid - left + 1;
-        int rightArrayLength = right - mid;
-        cout << "LeftArray length = " << leftArrayLength << endl;
-        cout << "Right Array length = " << rightArrayLength << endl;
-        int *leftArray = new int[leftArrayLength];
-        int *rightArray = new int[rightArrayLength];   
-        
-        for(int i = 0; i < leftArrayLength; ++i){
-            leftArray[i] = array[left + i];
-        }
-        for(int i = 0; i < rightArrayLength; ++i){
-            rightArray[i] = array[mid + i + 1];
-        }
-        int indexOfLeftArray = 0;
-        int indexOfRightArray = 0;
-        int indexOfMergedArray = left;
+    const int leftArrayLength = mid - left + 1;
+    const int rightArrayLength = right - mid;
+    int leftArray[leftArrayLength];
+    int rightArray[rightArrayLength];   
+    
+    for(int i = 0; i < leftArrayLength; ++i){
+        leftArray[i] = array[left + i];
+    }
+    for(int j = 0; j < rightArrayLength; ++j){
+        rightArray[j] = array[mid + j + 1];
+    }
+    int indexOfLeftArray = 0;
+    int indexOfRightArray = 0;
+    int indexOfMergedArray = left;
 
-        while(indexOfLeftArray < leftArrayLength && indexOfRightArray < rightArrayLength){
-            if(leftArray[indexOfLeftArray] < rightArray[indexOfRightArray]){
-                array[indexOfMergedArray] = leftArray[indexOfLeftArray];
-                ++indexOfLeftArray;
-            }
-            else{
-                array[indexOfMergedArray] = rightArray[indexOfRightArray];
-                ++indexOfRightArray;
-            }
-            ++indexOfMergedArray;
-        }
-        while(indexOfLeftArray < leftArrayLength){
+    while(indexOfLeftArray < leftArrayLength && indexOfRightArray < rightArrayLength){
+        if(leftArray[indexOfLeftArray] < rightArray[indexOfRightArray]){
             array[indexOfMergedArray] = leftArray[indexOfLeftArray];
             ++indexOfLeftArray;
-            ++indexOfMergedArray;
         }
-        while(indexOfRightArray < rightArrayLength){
+        else{
             array[indexOfMergedArray] = rightArray[indexOfRightArray];
             ++indexOfRightArray;
-            ++indexOfMergedArray;
         }
+        ++indexOfMergedArray;
+    }
+    while(indexOfLeftArray < leftArrayLength){
+        array[indexOfMergedArray] = leftArray[indexOfLeftArray];
+        ++indexOfLeftArray;
+        ++indexOfMergedArray;
+    }
+    while(indexOfRightArray < rightArrayLength){
+        array[indexOfMergedArray] = rightArray[indexOfRightArray];
+        ++indexOfRightArray;
+        ++indexOfMergedArray;
     }
 
 }
 void mergeSort(int array[], int begin, int end){  
-    if(begin >= end){
-        return;
+    cout << "begin : "<< begin << endl;
+    cout << "end : "<< end << endl;
+    for(int i = 0; i < (sizeof(&array)/sizeof(array[0])); ++i){
+        cout << array[i] << endl;
     }
-    int mid = (begin + (end - begin))/2;     
-    mergeSort(array, begin, mid);
-    mergeSort(array, mid+1, end);
-    merge(array, begin, mid, end);        
-    
+    if(begin < end){
+        int mid = (begin + end)/2;     
+        mergeSort(array, begin, mid);
+        mergeSort(array, mid+1, end);
+        merge(array, begin, mid, end);        
+    }
 }
 int main(){
-    int array[2] = { 10, 5};
-    mergeSort(array, 0, 2);
+    int array[3] = { 10, 5, 4};
+    mergeSort(array, 0, 3);
 
-    for(int i = 0; i < 2; ++i){
+    for(int i = 0; i < 3; ++i){
         cout << array[i];
     }
     return 0;
