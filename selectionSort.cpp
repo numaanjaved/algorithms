b8e0d889a981ee82681d5c5b12c70897a921587f
Numaan Javed <naumaan.javed@hotmail.com>
2022-04-02 13:08:55 +0500

    selectionSort update

---
+++ selectionSort.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+
+int select(int array[], int p, int r, int k){
+    if(p == r){
+        return array[p];
+    }
+    else{
+        int x = choosePivot(array, p, r);
+        int q = partition(array, p, r, x);
+        int rankX = q - p + 1;
+
+        if(k == rankX){
+            return x;
+        }
+        else if(k < rankX){
+            return select(array, p, q-1, k);
+        }
+        else{
+            return select(array, q+1, r, k);
+        }
+    }
+
+}
+
+int main(){
+    int array[8] = {5,9,2,6,4,1,3,7};
+    
+
+    return 0;
+}
